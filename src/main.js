eae5d560b94306096d8a6c03fa8d67df8e9c486e
develo-pyo <jungpyo9@gmail.com>
2020-06-05 15:25:46 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//https://www.youtube.com/watch?v=5lDEee-Ur34&list=PLB7CpjPWqHOtYP7P_0Ls9XNed0NLvmkAh&index=13
+
 new Vue({
   router,
   store,
